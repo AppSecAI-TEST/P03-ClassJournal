a25ae3bd992759c44a23528ab851a9f30c7d42d3
15004557 <H0peyur3gd7>
2017-05-04 12:13:14 +0800

    Intent from listview

--- app/src/main/java/p03_classjournal/android/myapplicationdev/com/p03_classjournal/MainActivity.java
+++ app/src/main/java/p03_classjournal/android/myapplicationdev/com/p03_classjournal/MainActivity.java
@@ -1,7 +1,10 @@
 package p03_classjournal.android.myapplicationdev.com.p03_classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
         aa = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, al);
         lv.setAdapter(aa);
         aa.notifyDataSetChanged();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                    i.putExtra("module_code", "C347");
+                    startActivity(i);
+
+
+
+            }
+        });
 
     }
 }
