6d7a4bb03f13cfe955b85c2ce32ae8edc5927434
dennywongso <15017096@myrp.edu.sg>
2017-05-04 13:33:00 +0800

    main2activity

--- app/src/main/java/p03_classjournal/android/myapplicationdev/com/p03_classjournal/Main2Activity.java
+++ app/src/main/java/p03_classjournal/android/myapplicationdev/com/p03_classjournal/Main2Activity.java
@@ -2,12 +2,46 @@ package p03_classjournal.android.myapplicationdev.com.p03_classjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
+    Button btnAdd;
+    ListView lvGrade;
+    ArrayAdapter aa;
+    ArrayList<String> module_grade;
+    ArrayList<ArrayList<String>> grade;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        lvGrade = (ListView)findViewById(R.id.lvGrade);
+        grade = new ArrayList<ArrayList<String>>();
+        module_grade = new ArrayList<String>();
+        module_grade.add("DG");
+        module_grade.add("C");
+        module_grade.add("asd");
+        grade.add(module_grade);
+
+
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+
+
 }
