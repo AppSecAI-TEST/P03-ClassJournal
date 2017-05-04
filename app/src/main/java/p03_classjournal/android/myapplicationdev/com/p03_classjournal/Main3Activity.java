2837a6b015171be0a7820ba28df9d56da5ba3442
15004557 <H0peyur3gd7>
2017-05-04 13:31:52 +0800

    Third Activity layout with image

--- app/src/main/java/p03_classjournal/android/myapplicationdev/com/p03_classjournal/Main3Activity.java
+++ app/src/main/java/p03_classjournal/android/myapplicationdev/com/p03_classjournal/Main3Activity.java
@@ -2,8 +2,12 @@ package p03_classjournal.android.myapplicationdev.com.p03_classjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
+
+    TextView tvWeek, tvDG;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
